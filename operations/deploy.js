8daa751fe219e07371b00b196ebef189467080b5
matst80 <mats.tornberg@gmail.com>
2023-03-10 17:44:06 +0100

    update actiondata

--- operations/deploy.js
+++ operations/deploy.js
@@ -1,91 +1,92 @@
 /** @type {import('../src/types').DeploymentConfig} */
-module.exports = async ({ createDeployment, createService, createIngress }, { sha }) => {
-	const namespace = 'default'
-	const labels = { app: 'yaml-converter' }
-	// const shortSha = sha.slice(0, 7)
+module.exports = async (
+  { createDeployment, createService, createIngress },
+  { sha }
+) => {
+  const namespace = "default";
+  const labels = { app: "yaml-converter" };
 
-	await createDeployment(namespace,
-		{
-			metadata: {
-				name: 'yaml-converter',
-				labels,
-			},
-			spec: {
-				replicas: 1,
-				selector: {
-					matchLabels: labels,
-				},
-				template: {
-					metadata: {
-						labels,
-					},
-					spec: {
-						imagePullSecrets: [
-							{
-								name: 'regcred',
-							},
-						],
-						containers: [
-							{
-								name: "converter",
-								image: 'registry.knatofs.se/yaml-converter:latest',
-								imagePullPolicy: "Always",
-								ports: [
-									{
-										containerPort: 8080,
-									},
-								],
-								resources: {
-									requests: {
-										memory: "40Mi",
-									},
-								},
-							},
-						],
-					},
-				},
-			},
-		})
-	await createService(namespace, {
-		metadata: {
-			name: "yaml-converter",
-		},
-		spec: {
-			selector: labels,
-			ports: [
-				{
-					port: 8080,
-				}
-			]
-		}
-	})
-	await createIngress(namespace, {
-		metadata: {
-			name: "yaml-converter",
-		},
-		spec: {
-			ingressClassName: "nginx",
-			rules: [
-				{
-					host: "yaml-converter.knatofs.se",
-					http: {
-						paths: [
-							{
-								path: "/",
-								pathType: "Prefix",
-								backend: {
-									service: {
-										name: "yaml-converter",
-										port: {
-											number: 8080,
-										},
-									},
-								},
-							},
-						],
-					},
-				},
-			],
-		},
-	})
-}
+  await createDeployment(namespace, {
+    metadata: {
+      name: "yaml-converter",
+      labels,
+    },
+    spec: {
+      replicas: 1,
+      selector: {
+        matchLabels: labels,
+      },
+      template: {
+        metadata: {
+          labels,
+        },
+        spec: {
+          imagePullSecrets: [
+            {
+              name: "regcred",
+            },
+          ],
+          containers: [
+            {
+              name: "converter",
+              image: "registry.knatofs.se/yaml-converter:latest",
+              imagePullPolicy: "Always",
+              ports: [
+                {
+                  containerPort: 8080,
+                },
+              ],
+              resources: {
+                requests: {
+                  memory: "40Mi",
+                },
+              },
+            },
+          ],
+        },
+      },
+    },
+  });
+  await createService(namespace, {
+    metadata: {
+      name: "yaml-converter",
+    },
+    spec: {
+      selector: labels,
+      ports: [
+        {
+          port: 8080,
+        },
+      ],
+    },
+  });
+  await createIngress(namespace, {
+    metadata: {
+      name: "yaml-converter",
+    },
+    spec: {
+      ingressClassName: "nginx",
+      rules: [
+        {
+          host: "yaml-converter.knatofs.se",
+          http: {
+            paths: [
+              {
+                path: "/",
+                pathType: "Prefix",
+                backend: {
+                  service: {
+                    name: "yaml-converter",
+                    port: {
+                      number: 8080,
+                    },
+                  },
+                },
+              },
+            ],
+          },
+        },
+      ],
+    },
+  });
+};
