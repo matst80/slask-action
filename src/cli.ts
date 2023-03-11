6c2090b38d365f284ef6f2eb605dbbd497f6e98d
matst80 <mats.tornberg@gmail.com>
2023-03-11 17:44:47 +0100

    cli stuff

--- src/cli.ts
+++ src/cli.ts
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
+
+import { ActionsCore, makeSlask } from "./slask-base";
+
 const [, , ...args] = process.argv;
 console.log(args);
+const fakeCore: ActionsCore = {
+  setOutput: (name: string, value: any) => console.log("Output:", name, value),
+  setFailed: (message: string) => console.error(message),
+  getInput: (name: string, options?: any) => process.env[name],
+  info: (message: string) => console.log(message),
+};
+makeSlask(fakeCore, args[0], JSON.parse(args[1] ?? "{}"));
--- src/slask.ts
+++ src/slask.ts
@@ -1,17 +1,9 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
-import * as k8s from "@kubernetes/client-node";
-import { DeploymentConfig } from "./types";
+
 import { join } from "path";
-import {
-  createConfigMap,
-  createDeployment,
-  createIngress,
-  createNamespace,
-  createPersistentVolumeClaim,
-  createSecret,
-  createService,
-} from "./apply";
+
+import { makeSlask } from "./slask-base";
 
 const scriptFile =
   core.getInput("scriptPath") ??
@@ -21,71 +13,7 @@ const scriptFile =
 const localPath = process.env.GITHUB_WORKSPACE ?? ".";
 core.info("Loading deployment script: " + scriptFile);
 
-const wrap =
-  <R>(type: string, fn: (...args: any[]) => Promise<R>) =>
-  (...args: any[]) => {
-    return fn(...args)
-      .then(({ body }: any) => {
-        if (body?.metadata?.name) {
-          core.setOutput(body?.metadata?.name, body);
-          core.info(`Applied ${type}: ${body.metadata.name}`);
-        }
-      })
-      .catch((e) => {
-        //core.warning(e);
-        core.setFailed(e);
-      });
-  };
-
 const file = join(localPath, scriptFile);
 core.info(file);
 
-export default import(file)
-  .then((m) => m.default as DeploymentConfig)
-  .then((deployment) => {
-    const kc = new k8s.KubeConfig();
-    const config = core.getInput("k8sConfig");
-
-    if (config) {
-      kc.loadFromString(config);
-    } else {
-      kc.loadFromDefault();
-    }
-
-    const k8sAppsApi = kc.makeApiClient(k8s.AppsV1Api);
-    const k8sCoreApi = kc.makeApiClient(k8s.CoreV1Api);
-    const k8sNetworkingApi = kc.makeApiClient(k8s.NetworkingV1Api);
-
-    return deployment(
-      {
-        createDeployment: wrap("deployment", (namespace, deployment) =>
-          createDeployment(namespace, deployment, k8sAppsApi)
-        ),
-        createService: wrap("service", (namespace, service) =>
-          createService(namespace, service, k8sCoreApi)
-        ),
-        createIngress: wrap("ingress", (namespace, data) =>
-          createIngress(namespace, data, k8sNetworkingApi)
-        ),
-        createPersistentVolumeClaim: wrap("volume claim", (namespace, data) =>
-          createPersistentVolumeClaim(namespace, data, k8sCoreApi)
-        ),
-        createConfigMap: wrap("configmap", (namespace, data) =>
-          createConfigMap(namespace, data, k8sCoreApi)
-        ),
-        createSecret: wrap("secret", (namespace, data) =>
-          createSecret(namespace, data, k8sCoreApi)
-        ),
-        createNamespace: wrap("namespace", (data) =>
-          createNamespace(data, k8sCoreApi)
-        ),
-      },
-      github.context
-    );
-  })
-  .catch((e) => {
-    core.setFailed(e);
-  })
-  .finally(() => {
-    core.info("Done");
-  });
+export default makeSlask(core, file, github.context);
